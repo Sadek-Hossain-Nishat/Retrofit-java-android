ce798e668f325bc240f6ee2e8d0ed26a03b5c9c2
Sadek-Hossain-Nishat <sadekhossainnishat@gmail.com>
2022-08-09 11:17:01 -0700

    simple get request

---
+++ app/src/main/java/com/example/retrofitapplication/Post.java
@@ -0,0 +1,41 @@
+package com.example.retrofitapplication;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Post {
+    private int userId;
+
+    private int id;
+
+    private String title;
+
+    @SerializedName("body")
+    private String text;
+
+
+
+//getter methods
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+
+
+
+
+
+
+}
